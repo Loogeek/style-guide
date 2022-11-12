40dadb6296e2fd7e07ca07ca9fe30767c6dce059
loogeek <xiaolong3956@gmail.com>
2022-11-12 12:20:16 +0800

    feat: update prettier config

---
+++ eslint/typescript.js
@@ -0,0 +1,24 @@
+const { TYPESCRIPT_FILES } = require('./constants');
+const requirePackage = require('./utils/require-package');
+
+requirePackage('typescript', 'typescript');
+
+module.exports = {
+  overrides: [
+    {
+      files: TYPESCRIPT_FILES,
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/strict',
+        'plugin:import/typescript',
+        'prettier',
+        // require.resolve('./rules/typescript'),
+        // require.resolve('./rules/typescript/extension'),
+        // require.resolve('./rules/typescript/import'),
+        // require.resolve('./rules/typescript/strict'),
+        // require.resolve('./rules/tsdoc'),
+      ],
+    },
+  ],
+};
---
+++ prettier/index.js
@@ -0,0 +1,19 @@
+/**
+ * Some of Prettier's defaults can be overridden by an EditorConfig file. We
+ * define those here to ensure that doesn't happen.
+ *
+ * See: https://github.com/prettier/prettier/blob/main/docs/configuration.md#editorconfig
+ */
+const overridableDefaults = {
+  endOfLine: "lf",
+  tabWidth: 2,
+  printWidth: 100,
+  useTabs: false,
+};
+
+module.exports = {
+  ...overridableDefaults,
+  singleQuote: true,
+  trailingComma: "all",
+  plugins: ["prettier-plugin-packagejson"],
+};
